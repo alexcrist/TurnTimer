cb8a9fb65309e3c22282e73a7e21f4e942b2b3e6
alexcrist <alexecrist@gmail.com>
2016-03-19 23:11:45 -0400

    Added basic android project scaffold

---
+++ app/src/main/java/com/alexcrist/turntimer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.alexcrist.turntimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
